2e07a1bdaef71c549499cf31f86e2aa6d8606612
Urbano dos Santos Silva Filho <urbano.filho@codevasf.gov.br>
2020-09-08 13:11:39 -0300

    first commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        return Product::all();
+    }
+
+    public function store(Request $request){
+        return Product::create($request->all());
+    }
+
+    public function update(Request $request, Product $product){
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function show(Product $product){
+        return $product;
+    }
+
+    public function destroy(Request $request, Product $product){
+        $product->delete();
+        return $product;
+    }
+}
